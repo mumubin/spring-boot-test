ca7c33a4f15d3b34f40b9f8ec0b1fb2c374c13de
胡彬 <hzhubin15@corp.netease.com>
2016-08-22 09:51:15 +0800

    misc:

--- src/main/java/config/controller/NameValueController.java
+++ src/main/java/config/controller/NameValueController.java
@@ -3,9 +3,11 @@ package config.controller;
 import config.domain.NameValue;
 import config.mapper.NameValueMapper;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Created by hzhubin15 on 2016/8/19.
@@ -20,4 +22,10 @@ public class NameValueController {
 	public NameValue query(String name){
 		return nameValueMapper.findByName(name);
 	}
+
+	@RequestMapping(value = "/session", method = RequestMethod.GET)
+	public String getSessionId(HttpServletRequest request) {
+		return request.getSession().getId();
+	}
+
 }
--- src/main/java/config/server/WebServer.java
+++ src/main/java/config/server/WebServer.java
@@ -1,5 +1,8 @@
 package config.server;
 
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -7,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 import java.security.Timestamp;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by hzhubin15 on 2016/8/19.
@@ -30,5 +34,20 @@ public class WebServer extends WebMvcConfigurerAdapter {
 				.setCachePeriod(1000);
 	}
 
-
+	/**
+	 * 设置session时间为一分钟
+	 * @return
+	 */
+	@Bean
+	public EmbeddedServletContainerCustomizer
+	embeddedServletContainerCustomizer() {
+		return new EmbeddedServletContainerCustomizer() {
+			@Override
+			public void
+			customize(ConfigurableEmbeddedServletContainer
+					container) {
+				container.setSessionTimeout(1, TimeUnit.MINUTES);
+			}
+		};
+	}
 }
